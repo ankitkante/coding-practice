80a2dba60d8c6fec21aa9d4d853af8fc80aedc31
Ankit Kante <ankit.kante@falkonry.com>
2025-09-08 14:28:05 +0530

    Fewer variables

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let currentElem = nums[0]
-    let anchor = 1
+    let anchor = 0
 
     for (let i = 1; i < nums.length; i++) {
         const elem = nums[i]
-        if (elem !== currentElem) {
-            currentElem  = elem
-            nums[anchor] = elem
+        if (elem !== nums[anchor]) {
             anchor++
+            nums[anchor] = elem
         }
     }
 
-    return anchor
+    return anchor+1
 };
 
 nums = [-23, -23, -18, -15, -15, -5, -5, -5, 0, 0, 5, 5, 5, 5, 77, 77]
