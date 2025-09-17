ba26a420de7af64da3258a7d47880ce02947790d
Ankit Kante <ankit.kante@falkonry.com>
2025-09-17 12:50:57 +0530

    Boyer Moore algorithm

--- majority-element.js
+++ majority-element.js
@@ -5,20 +5,19 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const countMap = {}
-    let majority
+    let count = 0, candidate = -10000000000000
     for(i=0;i<nums.length;i++){
-        const elem = nums[i]
-        if(countMap.hasOwnProperty(elem)){
-            countMap[elem] = countMap[elem] +1
-        } else {
-            countMap[elem] = 1
+        if(count ==0){
+            candidate = nums[i]
+            count = 1
+        } else{
+            if(nums[i] == candidate){
+                count++
+            } else {
+                count--
+            }
         }
+    }
 
-        if(countMap[elem] > Math.floor(nums.length/2)){
-            majority = elem
-        }
-    }  
-    
-    return majority
+    return candidate
 };
