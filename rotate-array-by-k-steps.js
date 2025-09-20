e5fcc57e7547d644c171240fe932ccb1a3bdd513
Ankit Kante <ankit.kante@falkonry.com>
2025-09-20 14:01:15 +0530

    Swapping algo to rotate array

--- rotate-array-by-k-steps.js
+++ rotate-array-by-k-steps.js
@@ -5,56 +5,35 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-// var rotate = function (nums, k) {
-//   let len = nums.length
-//   const sanitizedK = k % len
-//   i = 0
-//   nextElem = nums[i]
-//   for (let pass = 1; pass <= len; pass++) {
-//     if (pass >= Math.ceil(len / 2) && i == 0) {
-//       i = 1
-//       nextElem = nums[i]
-//     }
-//      console.log(`pass=${pass}`,`i=${i}`)
+var rotate = function (nums, k) {
+  let len = nums.length
+  const sanitizedK = k % len
+  
+  count = 0
+  for (let start = 0; count < len; start++) {
+    let current = start
+    nextElem = nums[start]
 
-//     let newIndex
-//     if (i <= (len - sanitizedK - 1)) {
-//       newIndex = i + sanitizedK
-//     } else {
-//       newIndex = i - (len - sanitizedK)
-//     }
+    do {
+      let newIndex = (current + sanitizedK) % len
+      // if (current <= (len - sanitizedK - 1)) {
+      //   newIndex = current + sanitizedK
+      // } else {
+      //   newIndex = current - (len - sanitizedK)
+      // }
 
-//     let newIndexItem = nums[newIndex]
-//     nums[newIndex] = nextElem
-//     i = newIndex
-//     nextElem = newIndexItem
-
-
-//     console.log(nums)
-//   }
-// }
-
-var rotate = function(nums, k) {
-  effectiveK = k % nums.length
-  reverse(nums, 0, nums.length-1)
-  reverse(nums, 0, effectiveK-1)
-  reverse(nums, effectiveK, nums.length-1)
-}
-
-var reverse = function(arr, start, end){
-  let leftIndex = start
-  let rightIndex = end
-
-  while(leftIndex < rightIndex){
-    const temp = arr[leftIndex]
-    arr[leftIndex] = arr[rightIndex]
-    arr[rightIndex] = temp
-    leftIndex++
-    rightIndex--
+      let temp = nums[newIndex]
+      nums[newIndex] = nextElem
+      current = newIndex
+      nextElem = temp
+      count++
+    } while (start !== current)
   }
 }
-        
-nums = [1,2,3,4,5,6,7]
+
+
+
+nums = [1, 2, 3, 4, 5, 6, 7]
 
 
 rotate(nums, 3)
