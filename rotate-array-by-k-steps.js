e90631c7721542744590a5f719a3a012a58ed0d7
Ankit Kante <ankit.kante@falkonry.com>
2025-09-20 12:47:28 +0530

    Rotation by reversal.

--- rotate-array-by-k-steps.js
+++ rotate-array-by-k-steps.js
@@ -5,21 +5,57 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var rotate = function (nums, k) {
+//   let len = nums.length
+//   const sanitizedK = k % len
+//   i = 0
+//   nextElem = nums[i]
+//   for (let pass = 1; pass <= len; pass++) {
+//     if (pass >= Math.ceil(len / 2) && i == 0) {
+//       i = 1
+//       nextElem = nums[i]
+//     }
+//      console.log(`pass=${pass}`,`i=${i}`)
+
+//     let newIndex
+//     if (i <= (len - sanitizedK - 1)) {
+//       newIndex = i + sanitizedK
+//     } else {
+//       newIndex = i - (len - sanitizedK)
+//     }
+
+//     let newIndexItem = nums[newIndex]
+//     nums[newIndex] = nextElem
+//     i = newIndex
+//     nextElem = newIndexItem
+
+
+//     console.log(nums)
+//   }
+// }
+
 var rotate = function(nums, k) {
-  for(var i=1;i<=k;i++){
-    shift(nums)
-  }  
-};
-
-var shift = function(nums){
-    lastElem = nums[nums.length-1]
-    for(var j=nums.length-2;j>=0;j--){
-        nums[j+1] = nums[j]
-    }
-
-    nums[0] = lastElem
+  effectiveK = k % nums.length
+  reverse(nums, 0, nums.length-1)
+  reverse(nums, 0, effectiveK-1)
+  reverse(nums, effectiveK, nums.length-1)
 }
 
+var reverse = function(arr, start, end){
+  let leftIndex = start
+  let rightIndex = end
+
+  while(leftIndex < rightIndex){
+    const temp = arr[leftIndex]
+    arr[leftIndex] = arr[rightIndex]
+    arr[rightIndex] = temp
+    leftIndex++
+    rightIndex--
+  }
+}
+        
 nums = [1,2,3,4,5,6,7]
+
+
 rotate(nums, 3)
 console.log(nums)
