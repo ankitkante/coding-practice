ff78d53bb1465b04bba827e2fc342797fa8b0226
Ankit Kante <ankit.kante@falkonry.com>
2025-09-25 18:47:09 +0530

    Solution that traverses from start to end

--- jump-game.js
+++ jump-game.js
@@ -4,16 +4,26 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var canJump = function(nums) {
+//     let target = nums.length - 1
+//     for(i = nums.length - 2; i>=0; i--){
+//         if(i + nums[i] >= target){
+//             target = i
+//         }
+//     }
+
+//     return target === 0
+// };
+
 var canJump = function(nums) {
-    let target = nums.length - 1
-    for(i = nums.length - 2; i>=0; i--){
-        if(i + nums[i] >= target){
-            target = i
-        }
+    let maxReach = 0
+    for(i=0;i<nums.length;i++){
+        if(i > maxReach) return false
+        maxReach = Math.max(maxReach, i + nums[i])
     }
 
-    return target === 0
-};
+    return true
+}
 
 
 nums = [2,5,0,0]
