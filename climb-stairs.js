60825f3de1e051c1a23f5d237460f3e45a90337e
Ankit Kante <ankit.kante@falkonry.com>
2025-10-01 22:31:39 +0530

    Approach 3: Bottom up or tabulation approach

--- climb-stairs.js
+++ climb-stairs.js
@@ -5,22 +5,18 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let memo = {}
-    return recur(n, memo)
-};
+    let dp = new Array(n+1)
 
-var recur = function(n, memo){
-    if(n==1) return 1
-    if(n==2) return 2
+    dp[0] = 1
+    dp[1] = 1
 
-    if(memo.hasOwnProperty(n)){
-        return memo[n]
+    for(let i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2]
     }
 
-    memo[n] =  recur(n-1, memo)+recur(n-2, memo)
+    return dp[n]
+};
 
-    return memo[n]
-}
 
-const n = 4
+const n = 2
 console.log(climbStairs(n))
