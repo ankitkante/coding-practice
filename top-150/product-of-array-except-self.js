dcafefc0d934d8a63172bc00e16cb7467787544d
Ankit Kante <ankit.kante@gmail.com>
2025-11-03 15:52:20 +0530

    Approach 1 using division

---
+++ top-150/product-of-array-except-self.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    // Approach 1: Using division (Doesn't work when a number is zero)
+    // let product = 1
+    // for(let i=0;i<nums.length;i++){
+    //     product = product * nums[i]
+    // }
+
+    // let result = []
+    // for(let j=0;j<nums.length;j++){
+    //     result[j] = product / nums[j]
+    // }
+
+    // return result
+
+    // Approach 2: 
+};
+
+const nums= [1,2,3,4]
+console.log(productExceptSelf(nums))
